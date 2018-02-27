8e4aed8a9fbc7f179c0bd2f5f97c8ac931cd7d4f
Gregory DONY <gregorydony@gmail.com>
2018-02-27 21:04:45 +0100

    Fix compilation error + upgrade to gradle 4.5.1

--- src/main/java/org/gisobject/rest/playground/UnknownLocation.java
+++ src/main/java/org/gisobject/rest/playground/UnknownLocation.java
@@ -1,12 +1,9 @@
 package org.gisobject.rest.playground;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Created by GIS Object on 23/02/2018.
  */
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Unable to find coordinates of the given resource")
+
 public class UnknownLocation extends RuntimeException {
     public UnknownLocation (String message) {
         super(message);
